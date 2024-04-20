ac14b6133fa69a3c854dbed4ed73b12f65ca3cd2
varissara wongprasit <varissara@techupth.com>
2024-04-20 16:46:40 +0700

    fix: refactor seperate tax calculator func

--- postgres/tax.go
+++ postgres/tax.go
@@ -2,70 +2,19 @@ package postgres
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/varissara-wo/assessment-tax/tax"
 )
 
-type TaxBracket struct {
-	MaxIncome float64
-	TaxRate   float64
-	MaxTax    float64
-}
-
-var taxBrackets = []TaxBracket{
-	{MaxIncome: 150000.0, TaxRate: 0.0, MaxTax: 0.0},
-	{MaxIncome: 500000.0, TaxRate: 0.1, MaxTax: 35000.0},
-	{MaxIncome: 1000000.0, TaxRate: 0.15, MaxTax: 110000.0},
-	{MaxIncome: 2000000.0, TaxRate: 0.2, MaxTax: 310000.0},
-	{MaxIncome: math.MaxFloat64, TaxRate: 0.35},
-}
-
-func calculateTax(income float64) float64 {
-	var tax float64
-	var previousMaxTax float64
-	var previousMaxIncome float64
-
-	for _, bracket := range taxBrackets {
-		if income <= bracket.MaxIncome {
-			tax = ((income - previousMaxIncome) * bracket.TaxRate) + previousMaxTax
-			break
-		}
-		previousMaxTax = bracket.MaxTax
-		previousMaxIncome = bracket.MaxIncome
-	}
-	return math.Round(tax)
-}
-
-func calculateAllowances(allowances []tax.Allowance) float64 {
-	personal := 60000.0
-	donation := 0.0
-	kreceip := 0.0
-
-	for _, a := range allowances {
-		if a.AllowanceType == "donation" {
-			donation += a.Amount
-		} else if a.AllowanceType == "k-receipt" {
-			kreceip += a.Amount
-		}
-	}
-
-	return personal + donation + kreceip
-}
-
-func calculateNetIncome(td tax.TaxDetails) float64 {
-	return td.TotalIncome - calculateAllowances(td.Allowances) - td.Wht
-}
-
 func (p *Postgres) TaxCalculation(td tax.TaxDetails) (tax.Tax, error) {
 
-	netIncome := calculateNetIncome(td)
+	netIncome := td.CalculateNetIncome()
 
 	if netIncome <= 0 {
 		return tax.Tax{Tax: fmt.Sprintf("%.1f", 0.0)}, nil
 	}
 
-	taxAmount := calculateTax(netIncome)
+	taxAmount := tax.CalculateTax(netIncome)
 
 	return tax.Tax{Tax: fmt.Sprintf("%.1f", taxAmount)}, nil
 }
--- tax/tax_test.go
+++ tax/tax_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -120,6 +121,86 @@ func TestValidateTaxDetails(t *testing.T) {
 		})
 	}
 
+}
+
+func TestTaxCalculation(t *testing.T) {
+
+	tests := []struct {
+		income float64
+		tax    float64
+	}{
+		{0.0, 0.0},
+		{150000.0, 0.0},
+		{150001.0, 0.0},
+		{500000.0, 35000.0},
+		{500001.0, 35000.0},
+		{1000000.0, 110000.0},
+		{1000001.0, 110000.0},
+		{2000000.0, 310000.0},
+		{3000000.0, 660000.0},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("final income %v should return %v", tt.income, tt.tax), func(t *testing.T) {
+			want := tt.tax
+
+			got := CalculateTax(tt.income)
+
+			if got != want {
+				t.Errorf("got %v want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestAllowancesCalculation(t *testing.T) {
+	t.Run("should return 60000", func(t *testing.T) {
+		want := 360000.0
+
+		mockAllowances := []Allowance{
+			{
+				AllowanceType: "k-receipt",
+				Amount:        200000.0,
+			},
+			{
+				AllowanceType: "donation",
+				Amount:        100000.0,
+			},
+		}
+
+		got := calculateAllowances(mockAllowances)
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestNetIncomeCalculation(t *testing.T) {
+	t.Run("should return 60000", func(t *testing.T) {
+		want := 638000.0
+
+		mockTaxDetails := TaxDetails{
+			TotalIncome: 1000000.0,
+			Wht:         2000.0,
+			Allowances: []Allowance{
+				{
+					AllowanceType: "k-receipt",
+					Amount:        200000.0,
+				},
+				{
+					AllowanceType: "donation",
+					Amount:        100000.0,
+				},
+			},
+		}
+
+		got := mockTaxDetails.CalculateNetIncome()
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
 
 type stub struct {
